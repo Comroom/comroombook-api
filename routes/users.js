52bbc6f6138fd5c53ef21fc699e51f4421127892
younghokim <donnaknew@gmail.com>
2016-02-12 17:27:53 +0900

    router.post login

--- routes/users.js
+++ routes/users.js
@@ -7,6 +7,26 @@ router.get('/', function(req, res, next) {
 });
 
 router.post('/login', function(req, res, next){
+  var session = req.session;
+  var body = req.body;
+  var inputs = {
+    email : body.email,
+    name : body.name,
+    password : body.password
+  };
+  users.find(inputs, function(err, docs){
+    if(err){
+      res.json({
+        error : err,
+        results : null
+      });
+    }else{
+      res.json({
+        error : null,
+        results : docs
+      });
+    }
+  });
 });
 
 router.post('/logout', function(req, res, next){
