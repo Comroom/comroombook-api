2dd331aab016feb47de197ab770921c32fb41bd3
InhoChoi <revaer@naver.com>
2016-02-12 16:37:37 +0900

    로그인/회원가입 기능 구현하기 위한 템플릿 작성

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var router = express.Router();
+var users = db['users'];
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+});
+
+router.post('/login', function(req, res, next){
+});
+
+router.post('/logout', function(req, res, next){
+});
+
+router.post('/signup', function(req, res, next){
 });
 
 module.exports = router;
