beb13620eddf73639f1692f243acbc5468563655
InhoChoi <revaer@naver.com>
2016-02-21 20:01:27 +0900

    시간표 GET/DELETE 부분

--- routes/time.js
+++ routes/time.js
@@ -4,11 +4,29 @@ var router = express.Router();
 var time = db['time'];
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  time.find({}, function(err, docs){
+    if(err){
+
+    }else{
+      res.json(docs);
+    }
+  });
 });
 
 router.post('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.delete('/',function(req, res, next) {
+  var body = req.body;
+  var id = req.time_id;
+
+  time.remove({ "_id" : id }, function(err, num) {
+    if(err){
+
+    }else{
+      res.json({ "result" : "삭제 성공하였습니다"});
+    }
+  });
+});
 module.exports = router;
