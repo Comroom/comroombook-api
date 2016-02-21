01f1e29af0dd42c08cd01d3c7a9c494f6fed91d0
InhoChoi <revaer@naver.com>
2016-02-21 19:38:39 +0900

    time.js 추가

---
+++ routes/time.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+var time = db['time'];
+
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+router.post('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
