f129bd84784bc43e171221432871348a7ded9a28
younghokim <donnaknew@gmail.com>
2016-02-21 20:01:41 +0900

    time post 추가

--- routes/time.js
+++ routes/time.js
@@ -7,8 +7,22 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/', function(req, res, next) {
+router.post('/time', function(req, res, next) {
   res.render('index', { title: 'Express' });
+
+  var body = req.body;
+  var session = req.session;
+
+  var inputs = {
+    day : body.day,
+    start : body.start,
+    end : body.end,
+    userid : body.userid,
+    title : body.title,
+    detail : body.detail
+  };
+
+  time.insert();
 });
 
 module.exports = router;
