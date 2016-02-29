8e7ff329eee66814a54213b28d2650a60c28d717
InhoChoi <revaer@naver.com>
2016-02-29 16:23:47 +0900

    메시지 기능 추가 GET

--- routes/message.js
+++ routes/message.js
@@ -1,13 +1,30 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/',function(req,res,next){
+var message = db['message'];
+var user = db['user'];
 
+router.get('/:user_id',function(req,res,next){
+  var user_id = req.params.user_id;
+
+  user.findOne({ "_id" : user_id },function(err,doc) {
+    if(err){
+
+    }else{
+      if( doc == null){
+        res.json({"error":"없는 아이디입니다."});
+      }
+      else{
+        message.find({ "receiver" : user_id },function(err,docs){
+          res.json(docs);
+        });
+      }
+    }
+  });
 });
 
 router.post('/',function(req,res,next){
 
 });
-
 
 module.exports = router;
